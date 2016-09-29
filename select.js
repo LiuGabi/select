d681dbda07e40404e91dae048ac7741823c48f2b
Gabi <gabi@eyebuydirect.com>
2016-09-29 19:48:08 +0800

    update

---
+++ select.js
@@ -0,0 +1,54 @@
+$.fn.uiSelect = function(options) {
+
+    var parent = $(this);
+
+    var opts = $.extend({}, $.fn.uiSelect.defaults, options);
+
+    var click = function(toggle, dropdown) {
+
+        parent.children(toggle).click(function(e) {
+
+            $(this).siblings(dropdown).show(function() {
+
+                selectSort($(this));
+
+                // console.log(this);
+
+            });
+
+        });
+    }
+
+    var selectSort = function(dropdown) {
+
+        dropdown.children().click(function() {
+
+            $(this).text();
+
+            var dataValue = $(this).attr("data-value");
+            
+            dropdown.siblings(opts.toggle).children().text($(this).text());
+            dropdown.siblings(opts.toggle).children().attr("data-value", dataValue);
+
+        });
+
+    }
+
+    var handleCallback = function(opts.callBackSelect) {
+
+        if (typeof opts.callBackSelect === "function") {
+
+                        
+        }
+
+    }
+
+    click(opts.toggle, opts.dropdown);
+
+};
+
+$.fn.uiSelect.defaults = {
+    toggle: '.dropdown-toggle',
+    dropdown: '.dropdown-list',
+    callBackSelect: function() {}
+};
